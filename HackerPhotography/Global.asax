0adb7775a42795cd323186f2e29b734797ce6608
Tim Hacker <tim.hacker@trueclarity.co.uk>
2011-12-21 13:11:32 +0000

    add visual studio project

---
+++ HackerPhotography/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackerPhotography.MvcApplication" Language="C#" %>
---
+++ HackerPhotography/Models/Photo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HackerPhotography.Models
+{
+	public class Photo
+	{
+		public int PhotoId { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public int SortOrder { get; set; }
+		public DateTime Date { get; set; }
+		[Required]
+		public string Url { get; set; }
+		public string AlternateDescription { get; set; }
+		public int Height { get; set; }
+		public int Width { get; set; }
+		public int AlbumId { get; set; }
+
+		public virtual Album Album { get; set; }
+	}
+}
